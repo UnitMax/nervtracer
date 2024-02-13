38acd931199145e1f8ed32bf12d4ff0f22b5b94f
UnitMax <105129888+UnitMax@users.noreply.github.com>
2024-02-13 21:40:19 +0100

    extend Vec3

--- src/main/java/unitmax/math/Vec3.java
+++ src/main/java/unitmax/math/Vec3.java
@@ -1,5 +1,7 @@
 package unitmax.math;
 
+import java.util.Arrays;
+
 public class Vec3 {
 
     private double e[] = new double[3];
@@ -46,12 +48,32 @@ public class Vec3 {
     }
 
     /**
+     * Subtracts another vector from the vector
+     * 
+     * @param v other vector
+     * @return a new vector containing the values of this vector - v
+     */
+    public Vec3 sub(Vec3 v) {
+        return new Vec3(this.x() - v.x(), this.y() - v.y(), this.z() - v.z());
+    }
+
+    /**
+     * Multiplies another vector with the vector
+     * 
+     * @param v other vector
+     * @return a new vector containing the values of this vector * v
+     */
+    public Vec3 mult(Vec3 v) {
+        return new Vec3(this.x() * v.x(), this.y() * v.y(), this.z() * v.z());
+    }
+
+    /**
      * Multiplies this vector with a scalar
      * 
      * @param s scalar
      * @return a new vector containing the values of this vector multiplied by s
      */
-    public Vec3 mult(double s) {
+    public Vec3 multScalar(double s) {
         return new Vec3(this.x() * s, this.y() * s, this.z() * s);
     }
 
@@ -61,23 +83,61 @@ public class Vec3 {
      * @param s scalar
      * @return a new vector containing the values of this vector divided by s
      */
-    public Vec3 div(double s) {
+    public Vec3 divScalar(double s) {
         return new Vec3(this.x() / s, this.y() / s, this.z() / s);
     }
 
     /**
+     * Calculates the dot product of this vector with another vector
+     * 
+     * @param v other vector
+     * @return the dot product of this vector and v
+     */
+    public double dotProduct(Vec3 v) {
+        return this.x() * v.x() + this.y() * v.y() + this.z() * v.z();
+    }
+
+    /**
+     * Calculates the cross product of this vector with another vector
+     * 
+     * @param v other vector
+     * @return the cross product of this vector and v
+     */
+    public Vec3 crossProduct(Vec3 v) {
+        return new Vec3(this.y() * v.z() - this.z() * v.y(), this.z() * v.x() - this.x() * v.z(),
+                this.x() * v.y() - this.y() - v.x());
+    }
+
+    /**
+     * Calculates the unit vector of this vector
+     * 
+     * @return this vector's unit vector
+     */
+    public Vec3 unitVector() {
+        return this.divScalar(this.length());
+    }
+
+    /**
      * Magnitude of the vector
+     * 
      * @return magnitude of the vector (sum of the squares of each component)
      */
     public double lengthSquared() {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 
     /**
      * Length of the vector
+     * 
      * @return length of the vector (square root of {@link #lengthSquared()})
      */
     public double length() {
         return Math.sqrt(lengthSquared());
     }
+
+    @Override
+    public String toString() {
+        return "Vec3 [e=" + Arrays.toString(e) + "]";
+    }
+
 }
