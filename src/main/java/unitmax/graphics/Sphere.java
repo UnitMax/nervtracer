db0eb75e9f7595c1dd0e52d0f9a60a20f534eead
UnitMax <105129888+UnitMax@users.noreply.github.com>
2024-02-19 22:47:53 +0100

    simplify Sphere.hit()

--- src/main/java/unitmax/graphics/Sphere.java
+++ src/main/java/unitmax/graphics/Sphere.java
@@ -8,14 +8,14 @@ public class Sphere {
 
     public static Optional<Double> hit(Vec3 center, double radius, Ray r) {
         Vec3 originCenterVector = r.getOrigin().sub(center);
-        var a = r.getDirection().dotProduct(r.getDirection());
-        var b = 2.0 * originCenterVector.dotProduct(r.getDirection());
-        var c = originCenterVector.dotProduct(originCenterVector) - radius * radius;
-        var discriminant = b * b - 4 * a * c;
+        var a = r.getDirection().lengthSquared();
+        var halfB = originCenterVector.dotProduct(r.getDirection());
+        var c = originCenterVector.lengthSquared() - radius * radius;
+        var discriminant = halfB * halfB - a * c;
         if (discriminant < 0) {
             return Optional.empty();
         }
-        return Optional.of((-b - Math.sqrt(discriminant)) / (2.0 * a));
+        return Optional.of((-halfB - Math.sqrt(discriminant)) / a);
     }
 
 }
